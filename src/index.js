eb4ed8823c356742f4d552e796d6a4d303fee7f6
xf4743939 <243630262@qq.com>
2021-04-24 08:55:53 +0800

    aaa

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import './index.css';
+// import './index.less';
+// function say(){
+//   return 'start learn webpack'
+// }
+
+// function sum (x,y){
+//   return x+y
+// }
+
+// console.log(say())
+// console.log(sum())
